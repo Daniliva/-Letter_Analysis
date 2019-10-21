742cb5dc1467e54fd0952a034e6355f2d1c0cf63
Daniliva <53309093+Daniliva@users.noreply.github.com>
2019-10-21 08:45:55 +0300

    some

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,31 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-   static Scanner in = new Scanner(System.in);
+    private static String inputStr() {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        return str;
+    }
+    private static double inputDouble() {
+        Scanner in = new Scanner(System.in);
+        double str = in.nextDouble();
+        return str;
+    }
+
+    private static void outputStr(String str) {
+        System.out.println(str);
+    }
      private static double Value(String side)
     { System.out.print("Input value for letter external "+side+": ");
-     //   Scanner in = new Scanner(System.in);
-    double a = in.nextDouble();
-   // in.close();
-
+    double a = inputDouble();
     return  a;
     }
     private static  boolean answer()
-    { // Scanner in = new Scanner(System.in);
-        String answer="y";
+    {
         String s2 = "Y";
-         in.nextLine();
-
-        answer = in.nextLine();
-      //  in.close();
+        String  answer = inputStr();
         if (Pattern.compile(Pattern.quote("yes"), Pattern.CASE_INSENSITIVE).matcher(answer).find()) {
             return true;
-
-
         } else if (Pattern.compile(Pattern.quote(s2), Pattern.CASE_INSENSITIVE).matcher(answer).find()) {
             return true;
         } else {
@@ -32,30 +36,25 @@ public class Main {
         }
 
     }
-    public static void main(String[] args) {
-
-        Letter letter_ab;
-        Letter letter_bc;
-        letter_ab = new Letter(1, 1);
-        letter_bc = new Letter("c", 1, "b", 1);
-      //  Scanner in = new Scanner(System.in);
+    private static void letterWhile(){
+        Letter   letter_ab = new Letter(1, 1);
+        Letter  letter_bc = new Letter("c", 1, "b", 1);
         boolean cal = true;
-
         while (cal) {
             letter_ab.setA(Value("a"));
             letter_ab.setB(Value("b"));
             letter_bc.setA(Value("c"));
             letter_bc.setB(Value("d"));
-            if (letter_ab.Envelope_Analysis(letter_bc)) {
+            if (letter_ab.compareTo(letter_bc)>0) {
                 System.out.println(letter_bc.toString() + " can invest " + letter_ab.toString());
             } else {
                 System.out.println(letter_bc.toString() + " can not invest " + letter_ab.toString());
             }
             System.out.println("You will continue?:\n");
             cal= answer();
-
         }
-in.close();
-
+    }
+    public static void main(String[] args) {
+        letterWhile();
     }
 }
