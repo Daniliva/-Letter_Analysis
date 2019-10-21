60d0df6b1e2858a4bc11f857d7253c749fa306f0
Daniliva <53309093+Daniliva@users.noreply.github.com>
2019-10-21 09:04:14 +0300

    some

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
         String str = in.nextLine();
         return str;
     }
+
     private static double inputDouble() {
         Scanner in = new Scanner(System.in);
         double str = in.nextDouble();
@@ -18,15 +19,17 @@ public class Main {
     private static void outputStr(String str) {
         System.out.println(str);
     }
-     private static double Value(String side)
-    { System.out.print("Input value for letter external "+side+": ");
-    double a = inputDouble();
-    return  a;
+
+    private static double Value(String side) {
+        outputStr("Input value for letter external " + side + ": ");
+        double a = inputDouble();
+        return a;
     }
-    private static  boolean answer()
-    {
+
+    private static boolean answer() {
+        outputStr("You will continue?:\n");
         String s2 = "Y";
-        String  answer = inputStr();
+        String answer = inputStr();
         if (Pattern.compile(Pattern.quote("yes"), Pattern.CASE_INSENSITIVE).matcher(answer).find()) {
             return true;
         } else if (Pattern.compile(Pattern.quote(s2), Pattern.CASE_INSENSITIVE).matcher(answer).find()) {
@@ -36,24 +39,29 @@ public class Main {
         }
 
     }
-    private static void letterWhile(){
-        Letter   letter_ab = new Letter(1, 1);
-        Letter  letter_bc = new Letter("c", 1, "b", 1);
-        boolean cal = true;
-        while (cal) {
-            letter_ab.setA(Value("a"));
-            letter_ab.setB(Value("b"));
-            letter_bc.setA(Value("c"));
-            letter_bc.setB(Value("d"));
-            if (letter_ab.compareTo(letter_bc)>0) {
-                System.out.println(letter_bc.toString() + " can invest " + letter_ab.toString());
-            } else {
-                System.out.println(letter_bc.toString() + " can not invest " + letter_ab.toString());
-            }
-            System.out.println("You will continue?:\n");
-            cal= answer();
+
+    private static void letterCreate() {
+        Letter letter_ab = new Letter(1, 1);
+        Letter letter_bc = new Letter("c", 1, "b", 1);
+        letter_ab.setA(Value("a"));
+        letter_ab.setB(Value("b"));
+        letter_bc.setA(Value("c"));
+        letter_bc.setB(Value("d"));
+        if (letter_ab.compareTo(letter_bc) > 0) {
+            outputStr(letter_bc.toString() + " can invest " + letter_ab.toString());
+        } else {
+            outputStr(letter_bc.toString() + " can not invest " + letter_ab.toString());
         }
     }
+
+    private static void letterWhile() {
+        boolean cal = true;
+        while (cal) {
+            letterCreate();
+            cal = answer();
+        }
+    }
+
     public static void main(String[] args) {
         letterWhile();
     }
